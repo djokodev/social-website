be161c8cca369ec5b79d4f76c10eb4944146ef5a
M@teo22 <djokodev75@gmail.com>
2025-01-13 13:30:13 +0100

    add custome backend authentication

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -51,6 +51,11 @@ MIDDLEWARE = [
 ]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "account.authentication.EmailAuthBackend",
+]
 
 ROOT_URLCONF = "bookmarks.urls"
 
@@ -119,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
